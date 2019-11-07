38251e84f8a77a1e96fe2a5583f2e615202a8476
AndreiMitrea <andrei.mitrea@imbuelabs.com>
2019-11-07 17:57:20 +0200

    Added visitor pages, improved footer

--- src/ElectionResults.WebApi/ClientApp/src/components/Footer/Footer.js
+++ src/ElectionResults.WebApi/ClientApp/src/components/Footer/Footer.js
@@ -1,32 +1,60 @@
 import React from "react";
 import { Container, Row, Col, Label, Input, Media } from "reactstrap";
 import code4Ro from "../../images/code4RoGrey.svg";
+import facebookLogo from "../../images/facebook_grey.png";
+import instagramLogo from "../../images/instagram_grey.png";
+import linkedinLogo from "../../images/linkedin_grey.png";
+import twitterLogo from "../../images/twitter_grey.png";
 import "./Footer.css";
 
 export function Footer() {
   return (
     <footer>
+      <Row>
+        <Col
+          className="social-media-container"
+        >
+          <a className="social-link-item" href="https://www.facebook.com/code4romania">
+            <img src={facebookLogo} width={48} height={48} alt="Facebook" />
+          </a>
+          <a className="social-link-item" href="https://www.instagram.com/code4romania">
+            <img src={instagramLogo} width={48} height={48} alt="Instagram" />
+          </a>
+          <a className="social-link-item" href="https://www.linkedin.com/company/code4romania">
+            <img src={linkedinLogo} width={48} height={48} alt="LinkedIn" />
+          </a>
+          <a className="social-link-item" href="https://twitter.com/Code4Romania">
+            <img src={twitterLogo} width={48} height={48} alt="Twitter" />
+          </a>
+          <a id="donate-link-button" href="https://code4.ro/ro/doneaza/">DONEAZĂ</a>
+        </Col>
+      </Row>
       <Container className="text-white footer">
         <Row>
-          <Col style={{ alignSelf: "flex-end" }}>
+          <Col
+            xs="12"
+            sm="6"
+            className="usefull-links"
+            style={{ alignSelf: "flex-end" }}
+          >
             <h4 className="link-title">Linkuri Utile</h4>
             <div>
-              <a className="link-item" href="#">
+              <a className="link-item" href="/termeni-si-conditii">
                 Termeni și condiții
               </a>
             </div>
             <div>
-              <a className="link-item" href="#">
+              <a className="link-item" href="/politica-de-confidentialitate">
                 Politica de confidențialitate
               </a>
             </div>
             <div>
-              <a className="link-item" href="#">
+              <a className="link-item" href="https://code4.ro/ro/codul-de-conduita">
                 Codul de conduita
               </a>
             </div>
             <div>
-              <a className="link-item" href="#">
+              <a className="link-item" href="https://code4.ro/ro">
                 Code for România
               </a>
             </div>
@@ -36,7 +64,10 @@ export function Footer() {
               </a>
             </div>
           </Col>
-          <Col>
+          <Col
+            xs="12"
+            sm="6"
+          >
             <div className="form-group">
               <Label className="link-title" for="email">
                 Abonează-te la newsletter
@@ -51,8 +82,8 @@ export function Footer() {
             <div className="logo-container">
               <Media src={code4Ro} />
             </div>
-            <div className="text-right mb-2">&copy; 2019 Code for Romania.</div>
-            <div className="text-right">
+            <div className="text-sm-right mb-2">&copy; 2019 Code for Romania.</div>
+            <div className="text-sm-right">
               Organizație neguvernamentală independentă, neafiliată politic și
               apolitică.
             </div>
